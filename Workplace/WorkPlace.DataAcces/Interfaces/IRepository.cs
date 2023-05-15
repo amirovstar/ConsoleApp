56490a7035e5d0d98b8cc6c0d05767d35e9a50f0
Furkan Amirli <130730661+amirovstar@users.noreply.github.com>
2023-05-16 01:53:54 +0400

    Updated WorkPlaces.DataAcces Interfaces

---
+++ Workplace/WorkPlace.DataAcces/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using WorkPlace.Core.Interfaces;
+
+namespace WorkPlace.DataAcces.Interfaces
+{
+    public interface IRepository<T> where T:IEntity
+	{
+        void Add(T entity);
+        void Update(int id, T entity);
+        void Delete(T entity);
+        T? GetById(int id);
+        List<T>? GetAll();
+        T? GetByName(string name);
+    }
+}
+
