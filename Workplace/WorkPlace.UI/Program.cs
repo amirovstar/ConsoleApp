08e76f99770f2a782766c0008539d91f11fa23b4
Furkan Amirli <130730661+amirovstar@users.noreply.github.com>
2023-05-16 11:11:19 +0400

    Added WorkPlace.UI

---
+++ Workplace/WorkPlace.UI/Program.cs
@@ -0,0 +1,12 @@
+﻿using WorkPlace.Business.DTOs;
+using WorkPlace.Business.Services;
+EmployeeService empservice = new EmployeeService();
+DepartmentService depservice = new DepartmentService();
+CompanyService compservice = new CompanyService();
+EmployeeDto empdto = new EmployeeDto(19999, "Furkan", "Amirli", 123);
+DepartmentDto depdto = new DepartmentDto("Furkan's Department",16,123);
+CompanyDto compdto = new CompanyDto("Furkan's Company");
+empservice.Create(empdto);
+depservice.Create(depdto);
+compservice.Create(compdto);
+
